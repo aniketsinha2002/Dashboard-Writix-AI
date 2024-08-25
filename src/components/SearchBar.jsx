8f20f19ab021f05ec28b3181cf0d6f4bddd5e278
Aniket <sinhaaniket192@gmail.com>
2024-08-25 18:10:38 +0530

    ui fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,7 @@ const SearchBar = ({ onSearch, isToggled }) => {
   return (
     <input
       type="text"
-      className={`rounded-md p-2 w-1/2 border-none focus:border-none focus:outline-none ${
+      className={`rounded-md p-2 md:w-1/2 border-none focus:border-none focus:outline-none ${
         isToggled ? "bg-[#2C2C2C] border-gray-700" : "bg-white text-black"
       }`}
       placeholder="Search..."
